90c6d1c30790d08a7e666a4d55007583a288b353
DON <booby9709@gmail.com>
2025-05-19 16:48:32 +0900

    Initial commit

---
+++ LT/LTApp.swift
@@ -0,0 +1,17 @@
+//
+//  LTApp.swift
+//  LT
+//
+//  Created by 이돈혁 on 5/19/25.
+//
+
+import SwiftUI
+
+@main
+struct LTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
